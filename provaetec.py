56b8cd2ccfc792c3650d6f97de54dc4bfa93e11c
LuisTinti <luisfelpstinti268@gmail.com>
2026-09-04 20:19:57 -0300

    codigo arrumado

--- provaetec.py
+++ provaetec.py
@@ -3,4 +3,4 @@ t = int(input("Quantas horas você trabalha?"))
 a = (t + 0.10)
 SF = S * (S * a * t)
 print(SF)
-
+# 0,10 ao invés de 0.10, por isso tava dando erro, mas agora ta certo, o problema era a vírgula, tem que ser ponto.
